b91340ff03fbcc2a44fa0f0f50ff39b6b7b2ccec
luuufan <angelia.fan@gmail.com>
2018-03-08 16:32:25 -0800

    add User.all, saving user by increase id

--- model/license_key.rb
+++ model/license_key.rb
@@ -12,7 +12,10 @@ class License_Key
   end
 
   def save
-
+    id
+    conn = PG::Connection.open(:dbname=>'shop_support_api')
+    conn.prepare('saveLicenseKey', 'INSERT INTO users (id, user_id, license_key) VALUES ($1, $2, $3)')
+    conn.exec_prepared('saveLicenseKey', [id, self.userId, self.license_key])
   end
 
   def self.find_by_userId(userId)
